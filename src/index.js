9cb419837d076c1b08ae974d0562310a271ee561
Retrospective53 <harry.pseu53@gmail.com>
2022-12-23 00:38:22 +0700

    added display city name after city input

--- src/index.js
+++ src/index.js
@@ -3,7 +3,50 @@
 console.log('magia');
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=ead3c9fd9ec85ca18a16b7bac31e0a80', {mode: 'cors'})
-.then(function(response) {
-    console.log(response.json());
-})
+const inputCity = document.getElementById('city');
+const buttonSubmit = document.getElementById('submit');
+const content = document.getElementById('content');
+let haha;
+
+const cityWeather = document.getElementById('cityName');
+const weatherName = document.getElementById('weatherName');
+const feelsLike = document.getElementById('feelsLike');
+const wind = document.getElementById('wind');
+const humidity = document.getElementById('humidity');
+
+const weatherInfo = document.getElementById('weather');
+weatherInfo.appendChild(cityWeather);
+weatherInfo.appendChild(weatherName);
+weatherInfo.appendChild(feelsLike);
+weatherInfo.appendChild(wind);
+weatherInfo.appendChild(humidity);
+
+function cityName(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ead3c9fd9ec85ca18a16b7bac31e0a80`, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        haha = response;
+        console.log(typeof response)
+        console.table(response)     
+        cityWeather.textContent = response.name;
+        weatherName.textContent = response.weather.main;
+        console.log(response.weather.main)
+    })
+}
+
+
+{/* <h1 id="cityName"></h1>
+                <h2 id="weatherName"></h2>
+                <h3 id="feelsLike"></h3>
+                <h3 id="wind"></h3>
+                <h3 id="humidity"></h3> */}
+
+ 
+
+
+
+// cityName('Jakarta');
+
+buttonSubmit.addEventListener('click', () => cityName(inputCity.value));
